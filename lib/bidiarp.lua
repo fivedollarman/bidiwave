fab0c385d97455d3a0262dfcc21aee845981c5f2
Marco Cinque <el5grana@gmail.com>
2021-09-28 22:28:59 +0200

    Update bidiarp.lua

--- lib/bidiarp.lua
+++ lib/bidiarp.lua
@@ -1,1 +1,22 @@
 local bidiarp = {}
+
+bidistep = 0
+countseq = 0
+
+function bidiarp.seq(num,den,list,offnum,offden)
+  engine.noteOffAll()
+  local seqvel = 1
+  local rlist = {table.unpack(list)}
+  while #list > 0 do
+      bidistep = (bidistep+1) % #rlist
+      countseq = (countseq+1) % 10
+      bidid = rlist[bidistep+1] + (1000*(countseq+1))
+      engine.noteOn(bidid,rlist[bidistep+1],seqvel)
+      clock.sync(offnum/offden)
+      engine.noteOff(bidid)
+      clock.sync((num/den)-(offnum/offden))
+  end
+  engine.noteOffAll()
+end
+
+return bidiarp
