1d1346d79030143d3f6218df82f47a1f608820f6
Marco Cinque <el5grana@gmail.com>
2021-09-30 23:38:29 +0200

    Update bidiarp.lua

--- lib/bidiarp.lua
+++ lib/bidiarp.lua
@@ -2,8 +2,6 @@ local bidiarp = {}
 
 bidistep = 0
 countseq = 0
-cycount = 0
-stepcount = 0
 
 function bidiarp.seq(num,den,list,dur,vel,listmode)
   engine.noteOffAll()
@@ -11,75 +9,63 @@ function bidiarp.seq(num,den,list,dur,vel,listmode)
   local dursync = (num/den) * dur
   local oct = 0
   local mute = 1
+  local skip = 0
+  local stepcount = 0
+  local cycount = 0
+
   while #list > 0 do
       countseq = (countseq+1) % 10
+      bidistep = (bidistep+1) % #rlist
       bidid = rlist[bidistep+1] + (1000*(countseq+1))
       engine.noteOn(bidid,rlist[bidistep+1]+(oct*12),vel*mute)
       clock.sync(dursync)
       engine.noteOff(bidid)
       clock.sync((num/den)-dursync)
-      bidistep = (bidistep+1) % #rlist
+      
       if listmode then -- arpeggiator algos
         if listmode["cyc"][1] == 1 then -- for cycles
-          oct = octcyc(bidistep,listmode["cyc"][2],listmode["cyc"][3])
-        elseif listmode["cyc"][1] == 2 then
-          bidistep = bidistep+skipcyc(bidistep,listmode["cyc"][2],listmode["cyc"][3])
-        else
-          oct = 0
+          if bidistep == 0 then
+            if cycount == listmode["cyc"][2]-1 then
+              oct = (oct+1)%listmode["cyc"][3]
+              cycount = cycount+1%listmode["cyc"][2]
+            end
         end
-        if listmode["step"][1] == 1 then -- for steps
-          bidistep = bidistep+skip(bidistep,listmode["step"][2],listmode["step"][3])
-        elseif listmode["step"][1] == 2 then
-          mute = mutestep(bidistep,listmode["step"][2],listmode["step"][3])
+        elseif listmode["cyc"][1] == 2 then
+          if bidistep == 0 then
+            if cycount == 0 then
+              skip = listmode["cyc"][3] 
+              bidistep = bidistep + skip
+              cycount = cycount+1%listmode["cyc"][2] 
+            else 
+              skip = 0
+            end
+        end
         else
           oct = 0
+          cycount = 0
+        end
+        
+        if listmode["step"][1] == 1 then -- for steps
+          if stepcount == 0 then
+            skip = listmode["step"][3]
+            bidistep = bidistep + skip
+            stepcount = stepcount+1%listmode["step"][2]
+          else 
+            skip = 0
+          end 
+        elseif listmode["step"][1] == 2 then
+          if stepcount == 0 then
+            mute = listmode["step"][3]/8
+            stepcount = stepcount+1%listmode["step"][2]
+          else 
+            mute = 1
+          end 
+        else
+          stepcount = 0
         end
       end
   end
   engine.noteOffAll()
-end
-
-function octcyc(step,every,nfor)
-  local octave = 0
-    if step == 0 then
-      if cycount == every-1 then
-        octave = (octave+1)%nfor
-        cycount = cycount+1%every
-      end
-    end
-  return octave
-end
-
-function skipcyc(step,every,skip)
-    if step == 0 then
-      if cycount == 0 then
-        skip = skip
-        cycount = cycount+1%every
-      else 
-        skip = 0
-      end
-    end
-  return skip
-end
-
-function skip(step,every,skip)
-    if stepcount == 0 then
-      skip = skip
-      stepcount = stepcount+1%every
-    else 
-      skip = 0
-    end 
-  return skip
-end
-
-function mutestep(step,every,mute)
-    if stepcount == 0 then
-      mute = mute/8
-      stepcount = stepcount+1%every
-    else 
-      mute = 1
-    end 
-  return mute
 end
 
 return bidiarp
